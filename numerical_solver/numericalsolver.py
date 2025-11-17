c73b8747fa75c67a93ed71453570eef746a14802
robertoarcanjo <jose.arcanjo@ee.ufcg.edu.br>
2025-11-17 15:14:12 -0300

    Update

--- numerical_solver/numericalsolver.py
+++ numerical_solver/numericalsolver.py
@@ -18,12 +18,26 @@ class NumericalSolver:
     def loadParams(self, jsonPath):
         with open(jsonPath, 'r') as f:
             params = json.load(f)
-        """self.wavelength = params["wavelength"] * self.micro_m
-        self.n_clad = params["n_clad"]
-        self.n_core = params["n_core"]
-        self.core_width = params["core_width"] * self.micro_m
-        self.core_height = params["core_height"] * self.micro_m
-        self.grid_size = params["grid_size"] * self.micro_m"""
         return params
     
+    def waveguideGeometry(self):
+
+        return
+    
+    def updateGrid(self):
+
+        return
+    
+    def computeEquations(self):
+
+        return
+    
+    def solve(self):
+
+        if self.params["solver"]["type"] == "yee-algorithm":
+            pass
+        else:
+            pass
+
+        return
     
